656266c8d967d0294362cd777c6e46f0881b91bb
github_julienmcoding <manois.julien@gmail.com>
2021-02-06 17:21:56 +0100

    story written

---
+++ main.js
@@ -0,0 +1,14 @@
+const hero = [];
+const monster = [];
+const monsterAttack = [];
+const heroAttack = [];
+
+console.log(`Long, long time ago, in a land far away, full of magic and surprising creatures, lived a mysterious people. 
+Their kingdom streched from the dark and cold forest of the north to the endless sea of the south. 
+A day, in a small village, surronded by mountains, a little ${hero} was playing with a friend named Elvy, calmly. 
+Suddenly, a gigantic ${monster} appeared. He took Elvy in his ${monsterAttack} and ran away.
+But he didn't know that our little ${hero} had magical powers.
+Within a second, ${heroAttack} killed the monster, Elvy was saved and they returned to their game.
+Even now they still hang around as they never got old.`);
+
+
