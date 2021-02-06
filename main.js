7ce39df20c321c30e90cbbd39b66a1c08a028abb
github_julienmcoding <manois.julien@gmail.com>
2021-02-06 17:39:48 +0100

    const created

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const hero = [];
-const monster = [];
-const monsterAttack = [];
-const heroAttack = [];
+const hero = ['princess', 'cat', 'knight', 'bird', 'elf'];
+const monster = ['dragon', 'pterodactyl', 'ogre', 'demon', 'vampire'];
+const monsterAttack = ['in its giant mouth', 'on his shoulder'];
+const heroAttack = ['screamed like hell', 'launched meteors', 'throwed a sharp weapon', 'sand a deadly song'];
 
 console.log(`Long, long time ago, in a land far away, full of magic and surprising creatures, lived a mysterious people. 
 Their kingdom streched from the dark and cold forest of the north to the endless sea of the south. 
 A day, in a small village, surronded by mountains, a little ${hero} was playing with a friend named Elvy, calmly. 
-Suddenly, a gigantic ${monster} appeared. He took Elvy in his ${monsterAttack} and ran away.
+Suddenly, a gigantic ${monster} appeared. He took Elvy ${monsterAttack} and ran away.
 But he didn't know that our little ${hero} had magical powers.
-Within a second, ${heroAttack} killed the monster, Elvy was saved and they returned to their game.
+Within a second, ${heroAttack} and killed the monster, Elvy was saved and they returned to their game.
 Even now they still hang around as they never got old.`);
 
 
